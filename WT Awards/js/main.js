1e5495cb5106bbf36b12d4fc21fc051159f8daaf
g-keeper <keep.cache@gmail.com>
2025-05-08 10:32:53 +0700

    Update main.js

--- WT Awards/js/main.js
+++ WT Awards/js/main.js
@@ -1,4 +1,4 @@
 const load_body = document.querySelector('.load');
 const auth_body = document.querySelector('.auth');
 
-window.onload = () => auth_body.classList.add('active');
+auth_body.classList.add('active');
