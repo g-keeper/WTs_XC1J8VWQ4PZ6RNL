2ab66443843a177d17c57a57fee5941a7bf34ce3
g-keeper <keep.cache@gmail.com>
2025-05-08 10:31:00 +0700

    Update main.js

--- WT Awards/js/main.js
+++ WT Awards/js/main.js
@@ -1,14 +1,4 @@
 const load_body = document.querySelector('.load');
 const auth_body = document.querySelector('.auth');
 
-function custom_timeout(delay) {
-  return new Promise(res => setTimeout(res, delay));
-}
-
-window.onload = async() => {
-
-  await custom_timeout(200);
-
-  auth_body.classList.add('active');
-
-}
+window.onload = () => auth_body.classList.add('active');
